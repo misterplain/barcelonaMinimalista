f8314634a8dc866b3c9cbd14ab74126d831d21b2
Patrick O'Brien <obrienp2321@gmail.com>
2022-02-13 10:42:12 +0100

    final push commit

--- js/script.js
+++ js/script.js
@@ -57,3 +57,86 @@ function myFunctionTrade() {
             }
         reverseOrder();
         }
+
+/*suggest button*/
+
+const suggestButton = document.body.querySelector("#suggestButton")
+const suggestButton2 = document.body.querySelector("#suggestButton2")
+const suggestButton3 = document.body.querySelector("#suggestButton3")
+const suggestButton4 = document.body.querySelector("#suggestButton4")
+const suggestButton5 = document.body.querySelector("#suggestButton5")
+const collabButton = document.body.querySelector("#collabButton")
+const submitted = document.body.querySelector("#submitted")
+
+$(function() {
+    $("#suggestButton").click(function() {
+        $("#suggestionModal").modal("show");
+    });
+})
+$(function() {
+    $("#suggestButton2").click(function() {
+        $("#suggestionModal").modal("show");
+    });
+})
+$(function() {
+    $("#suggestButton3").click(function() {
+        $("#suggestionModal").modal("show");
+    });
+})
+$(function() {
+    $("#suggestButton4").click(function() {
+        $("#suggestionModal").modal("show");
+    });
+})
+$(function() {
+    $("#suggestButton5").click(function() {
+        $("#suggestionModal").modal("show");
+    });
+})
+$(function() {
+    $("#collabButton").click(function() {
+        $("#collabModal").modal("show");
+    });
+})
+$(function() {
+    $("#submitted").click(function() {
+        $("#suggestionModal").modal("hide");
+        $("#submittedModal").modal("show");
+    });
+})
+
+
+/*water footprint script */
+const kilometers = document.body.querySelector("#kilometers");
+    const fastFashion = document.body.querySelector("#fastFashion");
+    const meat = document.body.querySelector("#meat");
+    const fly = document.body.querySelector("#fly");
+    const calculate = document.body.querySelector("#calculate");
+    const reset = document.body.querySelector("#reset");
+    calculate.addEventListener('click', totalWater);
+    reset.addEventListener('click', clearForm);
+    let total = 0
+
+
+    function totalWater () {
+        //kilometers per week at 1.2 gal/km, FF per week at 5,250 avg year, meat per week 125 gal/serving, fly 24 per week if x amount of hours per year 
+        total = total + (kilometers.value * 1.2) + (fastFashion.value * 100.96) + (meat.value * 125) + (fly.value * 24);
+        $("#footprintModal").modal("show");
+        let monthly = total * 4
+        let yearly = total * 52
+        let totalDisplay = "Your weekly water footprint is " + total + " gallons" 
+        let monthlyDisplay = "Which is " + monthly + " over the course of a month"
+        let yearlyDisplay = "And " + yearly + " over the course of a year"
+        $("#footprintText").html(totalDisplay);
+        $("#monthlyText").html(monthlyDisplay);
+        $("#yearlyText").html(yearlyDisplay);
+    }
+
+    function clearForm() {
+        console.log("attempt");
+        kilometers.value = "";
+        fastFashion.value = "";
+        meat.value = "";
+        fly.value = "";
+        total = 0;
+    }
